d8c6ba098fd559fa3bcdb6fdd94a5403c3de6144
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-01-13 20:37:53 -0800

    adjusting.

--- bin/git-dfs.sh
+++ bin/git-dfs.sh
@@ -2,5 +2,5 @@
 # https://stackoverflow.com/questions/14846967/git-submodule-foreach-robust-way-to-recursively-commit-a-child-module-first
 SCRIPT=$(realpath "$0")
 SCRIPTDIR=$(dirname "$SCRIPT")
-git submodule foreach "$SCRIPT $* && $*"
+git submodule foreach "$SCRIPT $*"
 $*
