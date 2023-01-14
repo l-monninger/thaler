dacf75ffd846cf5c7155874fe451b279cd141e84
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-01-13 19:05:29 -0800

    adding scripts.

--- bin/git-commit-all.sh
+++ bin/git-commit-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # intended to be executed from top of dir
-./bin/git-dfs.sh $(pwd && date > .date && git add . && git commit -a -m "${1}")
+./bin/git-dfs.sh '$(pwd && date > .date && git add . && git commit -a -m "${1}")'
 git add .
 git commit -m "$1"
